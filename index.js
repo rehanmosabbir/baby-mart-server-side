7f3d87defd66a4249e6d7830bbdc912c0c9b3e95
rehanmosabbir <rehanmosabbir@gmail.com>
2021-11-10 09:42:58 +0600

    setup database connection

--- index.js
+++ index.js
@@ -1,6 +1,34 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
+require("dotenv").config();
+const { MongoClient } = require("mongodb");
+
 const port = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.luhyz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+console.log(uri);
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const database = client.db("baby_mart");
+    console.log("database connected successfully");
+  } catch {
+    //   await client.close();
+  }
+}
+
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
